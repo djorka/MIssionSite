a81deb1bfaadb2b0a424bdd2d7bb646162219baf
Zach Eddington <zacheddington@gmail.com>
2017-11-12 20:24:11 -0700

    Added Brazil, Maceio mission

--- MissionSite/MissionSite/Controllers/MissionController.cs
+++ MissionSite/MissionSite/Controllers/MissionController.cs
@@ -13,7 +13,8 @@ namespace MissionSite.Controllers
         {
             new Mission("Mexico Reynosa", "Jorge Alejandro Regalado Campos", "Mexico Reynosa Mission <br />Av. Fundadores #100 Esq. con Entresuelo <br />Col. Ribereña <br />Reynosa, Tamaulipas <br />Mexico C.P. 88620", "Spanish", "Reynosa has a semi-arid climate with short, mild winters and hot summers", "Catholicism", "~/Content/Images/mexicoFlag.png"),
             new Mission("Nevada Las Vegas", "President D. Jack Snow", "9270 South Maryland Parkway <br />Las Vegas, NV 89123", "English", "Las Vegas has a subtropical hot desert climate, typical of the Mojave Desert in which it lies. This climate is typified by long, very hot summers; warm transitional seasons; and short, mild to chilly winters.", "Atheism", "~/Content/Images/nevadaFlag.png"),
-            new Mission("Oklahoma Oklahoma City", "Darren J. Mansell", "416 SW 79th St Ste 210 <br />Oklahoma City, OK 73139", "English", "Oklahoma City has a temperate humid subtropical climate, with frequent variations in weather daily and seasonally, except during the consistently hot and humid summer months.", "Baptist", "~/Content/Images/oklahomaFlag.png")
+            new Mission("Oklahoma Oklahoma City", "Darren J. Mansell", "416 SW 79th St Ste 210 <br />Oklahoma City, OK 73139", "English", "Oklahoma City has a temperate humid subtropical climate, with frequent variations in weather daily and seasonally, except during the consistently hot and humid summer months.", "Baptist", "~/Content/Images/oklahomaFlag.png"),
+            new Mission("Brazil Maceió", "Mark W. Taylor", "Av. Santa Rita de Cassia, 287 <br /> Educação Maceió Work Center <br />Farol, Maceió - AL <br />CEP f57051-600", "Portuguese", "Maceió has a typical tropical climate, specifically a tropical monsoon climate, with very warm to hot temperatures and high relative humidity all throughout the year.", "Catholicism", "~/Content/Images/brazilFlag.png")
         };
 
         // GET: Mission
